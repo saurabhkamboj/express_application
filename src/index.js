c0e9ea0ed8d705a83a14ae06db76b4137b8dd956
Saurabh Kamboj <skamboj97@gmail.com>
2025-08-14 02:06:35 +0530

    Modify files

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
 import 'dotenv/config';
 
+import express from 'express';
+
+const app = express();
+
+app.listen(3000, () => 
+  console.log('Personal app listening on port 3000!'),
+);
+
 console.log('An ever running Node.js personal project.');
 console.log(process.env.MY_SECRET);
