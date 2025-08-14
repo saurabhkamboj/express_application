686593667d0d34b0def2a8f93753270ef5456f52
Saurabh Kamboj <skamboj97@gmail.com>
2025-08-14 06:15:56 +0530

    Modify file

--- src/index.js
+++ src/index.js
@@ -4,9 +4,10 @@ import express from 'express';
 
 const app = express();
 
+app.get('/', (req, res) => {
+  res.send('Hello world!');
+});
+
 app.listen(3000, () => 
   console.log('Personal app listening on port 3000!'),
 );
-
-console.log('An ever running Node.js personal project.');
-console.log(process.env.MY_SECRET);
